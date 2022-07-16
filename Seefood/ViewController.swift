eb755dca87455ddfbd94df30302edb970b10dc9c
Wallace Santos <wallace_bcc@hotmail.com>
2022-07-15 23:51:06 -0300

    Aprendendo o uso do CoreML replicando o aplicativo da série Silicon Valley

--- Seefood/ViewController.swift
+++ Seefood/ViewController.swift
@@ -6,14 +6,66 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let imagePicker = UIImagePickerController()
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imageView.image = userPickedImage
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert to CI Image")
+            }
+            detect(image: ciImage)
+        }
+        imagePicker.dismiss(animated: true)
+        
+        
+    }
+    
+    func detect(image: CIImage){
+        
+        guard let model = try? VNCoreMLModel(for: MobileNetV2().model) else {
+            fatalError("Loading CoreML module failed")
+        }
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Failed to featch results")
+            }
+            if let firstResult = results.first{
+                self.navigationItem.title = firstResult.identifier.capitalized
+                print(firstResult.confidence.binade)
+            }
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+            do{
+                try handler.perform([request])
+            }
+        catch{
+            print(error)
+        }
+        
     }
 
-
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true)
+    }
+    
+    
+    
+    
 }
 
